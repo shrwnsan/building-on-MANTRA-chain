613a0aad88f1469d598baebd57f083e4f5aa31ee
shrwnsan <splash.contour.0w@icloud.com>
2025-01-25 02:46:29 +0800

    add-plus-query

--- contract/src/contract.rs
+++ contract/src/contract.rs
@@ -11,6 +11,7 @@ use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, ListResponse, QueryMsg};
 use crate::state::{Entry, Priority, Status, ENTRY_SEQ, LIST};
 
+// version info for migration
 const CONTRACT_NAME: &str = "to-do-list";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -21,10 +22,103 @@ pub fn instantiate(
     _info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
+    // First validate the address to ensure it's correct
     let validated_owner = deps.api.addr_validate(&msg.owner)?;
+
+    // Set contract version
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    // Initialize counter
     ENTRY_SEQ.save(deps.storage, &0u64)?;
+
+    // Create response with minimal attributes
     Ok(Response::default()
         .add_attribute("action", "instantiate")
         .add_attribute("owner", validated_owner.as_str()))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    match msg {
+        ExecuteMsg::NewEntry {
+            description,
+            priority,
+            owner,
+        } => execute_create_new_entry(deps, info, description, priority, owner),
+        ExecuteMsg::UpdateEntry {
+            id,
+            description,
+            status,
+            priority,
+            owner,
+        } => execute_update_entry(deps, info, id, description, status, priority, owner),
+        ExecuteMsg::DeleteEntry { id, owner } => execute_delete_entry(deps, info, id, owner),
+    }
+}
+
+pub fn execute_create_new_entry(
+    deps: DepsMut,
+    _info: MessageInfo,
+    description: String,
+    priority: Option<Priority>,
+    owner: String,
+) -> Result<Response, ContractError> {
+    let id = ENTRY_SEQ.update::<_, cosmwasm_std::StdError>(deps.storage, |id| Ok(id.add(1)))?;
+    let new_entry = Entry {
+        id,
+        description,
+        status: Status::ToDo,
+        priority: priority.unwrap_or(Priority::None),
+        owner,
+    };
+    LIST.save(deps.storage, id, &new_entry)?;
+    Ok(Response::new()
+        .add_attribute("method", "execute_create_new_entry")
+        .add_attribute("new_entry_id", id.to_string()))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::QueryUserList {
+            user,
+            start_after,
+            limit,
+        } => to_json_binary(&query_user_list(deps, user, start_after, limit)?),
+    }
+}
+
+const MAX_LIMIT: u32 = 30;
+const DEFAULT_LIMIT: u32 = 10;
+
+pub fn query_user_list(
+    deps: Deps,
+    user: String,
+    start_after: Option<u64>,
+    limit: Option<u32>,
+) -> StdResult<ListResponse> {
+    let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+    let start = start_after.map(Bound::exclusive);
+
+    let entries: StdResult<Vec<_>> = LIST
+        .range(deps.storage, start, None, Order::Ascending)
+        .filter(|item| {
+            if let Ok((_, entry)) = item {
+                entry.owner == user
+            } else {
+                false
+            }
+        })
+        .take(limit)
+        .collect();
+
+    let result = ListResponse {
+        entries: entries?.into_iter().map(|(_, entry)| entry).collect(),
+    };
+    Ok(result)
 }
